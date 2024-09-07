6fccc3b4134dfb14bce440b5ff075e991a02dc58
iodtp <iltoa7707@gmail.com>
2024-09-07 10:46:37 -0400

    death from spikes is 90% working

--- trainingbackup.js
+++ trainingbackup.js
@@ -17,6 +17,34 @@ const WALL_FRICTION = 0.5;
 const WIDTH = Math.min(1280, window.innerWidth);
 const HEIGHT = Math.min(800, window.innerHeight);
 
+const entityCategory = {
+    RED_BALL :          0x0001,
+    BLUE_BALL :     0x0002,
+    RED_ENDZONE :        0x0004,
+    BLUE_ENDZONE : 0x0008,
+    RED_TEAMTILE :    0x0010,
+    BLUE_TEAMTILE :    0x0020,
+    YELLOW_TEAMTILE :      0x0040,
+    RED_FLAG :    0x0080,
+    BLUE_FLAG :    0x0100,
+    YELLOW_FLAG :      0x0200,
+    SPIKE :    0x0400,
+    RED_BOOST : 0x0800,
+    BLUE_BOOST : 0x1000,
+    YELLOW_BOOST : 0x2000,
+    RED_PORTAL : 0x4000,
+    BLUE_PORTAL : 0x8000,
+    PORTAL : 0x10000,
+    BUTTON : 0x20000,
+    RED_GATE : 0x40000,
+    BLUE_GATE : 0x80000,
+    GREEN_GATE : 0x100000,
+    ROLLING_BOMB : 0x200000,
+    JUKE_JUICE : 0x400000,
+    TAGPRO : 0x800000,
+    BOMB : 0x1000000,
+  };
+
 (function() {
     'use strict';
     const nav = document.getElementById('nav-feedback');
@@ -85,35 +113,45 @@ function training(tiles){
     for(let i = 0; i < map.length; i++){
         mapSprites.push([]);
         for(let j = 0; j < map[i].length; j++){
-            if(map[i][j] != '0'){ //blank space
+            const current_tile = map[i][j];
+            if(current_tile != '0'){ //blank space
                 const x = i * 40;
                 const y = j *40;
-                mapSprites[i].push(addSpriteToLocation(app, tiles, map[i][j], x, y, map, i, j));
-                if(map[i][j] === '1') {
+                mapSprites[i].push(addSpriteToLocation(app, tiles, current_tile, x, y, map, i, j));
+                if(current_tile === '1') {
                     createWall(x,y,40,40, world);
                 }
-                else if (map[i][j] === '1.1'){
+                else if (current_tile === '1.1'){
                     const vertices = [new Box2D.Common.Math.b2Vec2(0, 1), new Box2D.Common.Math.b2Vec2(0, 0), new Box2D.Common.Math.b2Vec2(1, 1)];
                     createNonSquareWall(x, y, vertices, world);
                 }
-                else if (map[i][j] === '1.2'){
+                else if (current_tile === '1.2'){
                     const vertices = [new Box2D.Common.Math.b2Vec2(0, 1), new Box2D.Common.Math.b2Vec2(0, 0), new Box2D.Common.Math.b2Vec2(1, 0)];
                     createNonSquareWall(x, y, vertices, world);
                 }
-                else if (map[i][j] === '1.3'){
+                else if (current_tile === '1.3'){
                     const vertices = [new Box2D.Common.Math.b2Vec2(0, 0), new Box2D.Common.Math.b2Vec2(1, 0), new Box2D.Common.Math.b2Vec2(1, 1)];
                     createNonSquareWall(x, y, vertices, world);
                 }
-                else if (map[i][j] === '1.4'){
+                else if (current_tile === '1.4'){
                     const vertices = [new Box2D.Common.Math.b2Vec2(1, 0), new Box2D.Common.Math.b2Vec2(1, 1), new Box2D.Common.Math.b2Vec2(0, 1)];
                     createNonSquareWall(x, y, vertices, world);
+                }
+                else if (current_tile === '7') {
+                    createSpike(x,y,14,world);
                 }
             }
         }
     }
+
     const playerSprite = addSpriteToLocation(app, tiles, 'redball', 120, 120);
-    playerSprite.anchor.set(0.5,0.5);
     const playerCollision = createBall(120, 120, 19, world);
+    const player = {};
+    playerSprite.anchor.set(0.5,0.5);
+    player.playerSprite = playerSprite;
+    player.playerCollision = playerCollision;
+    player.hasFlag = false;
+    player.dead = false;
 
     const keys = {
         up: false,
@@ -122,10 +160,6 @@ function training(tiles){
         right: false
     };
 
-        //gonna change all this to modify object values like velocity and stuff
-        //then use those values in loop?
-        //also make variables to remember keydown and add another event for up
-        //unsure if thats gonna work how i want it to but well see ig
     document.addEventListener('keydown', (event) => {
         switch (event.key) {
             case 'ArrowRight': // Move right
@@ -163,7 +197,34 @@ function training(tiles){
         }
     });
 
-    app.ticker.add(delta => loop(delta, playerSprite, playerCollision, world, keys, app));
+    // Implement the contact listener
+    let listener = new Box2D.Dynamics.b2ContactListener();
+
+    // Called when two fixtures begin to overlap
+    listener.BeginContact = function(contact) {
+        let fixtureA = contact.GetFixtureA();
+        let fixtureB = contact.GetFixtureB();
+
+
+        // Check if both of the fixtures are
+        if (fixtureA.IsSensor() || fixtureB.IsSensor()) {
+            let type1 = fixtureA.GetBody().GetUserData().type;
+            let type2 = fixtureB.GetBody().GetUserData().type;
+            if(type1 === 'redball') {
+                switch(type2){
+                    case '7':
+                        console.log("dead");
+                        player.dead = true;
+                }
+            }
+            // Handle your sensor logic here
+        }
+    };
+
+    // Assign the listener to the world
+    world.SetContactListener(listener);
+
+    app.ticker.add(delta => loop(delta, player, world, keys, app));
 }
 
 function createWall(x, y, width, height, world) {
@@ -181,6 +242,8 @@ function createWall(x, y, width, height, world) {
     fixtureDef.friction = WALL_FRICTION;
 
     wallBody.CreateFixture(fixtureDef);
+
+    wallBody.SetUserData({type: "1"});
 }
 
 function createNonSquareWall(x,y,vertices, world) {
@@ -199,6 +262,26 @@ function createNonSquareWall(x,y,vertices, world) {
 
 
     wallBody.CreateFixture(fixtureDef);
+
+    wallBody.SetUserData({type: "1"}); //for our purposes all types of walls are 1
+}
+
+function createSpike(x, y, radius, world) {
+    const bodyDef = new Box2D.Dynamics.b2BodyDef();
+    bodyDef.type = Box2D.Dynamics.b2Body.b2_staticBody;
+    bodyDef.position.Set((x+20) / 40, (y+20) / 40);
+
+    const dynamicBody = world.CreateBody(bodyDef);
+
+    const circleShape = new Box2D.Collision.Shapes.b2CircleShape();
+    circleShape.SetRadius(radius / 40);
+
+    const sensorDef = new Box2D.Dynamics.b2FixtureDef();
+    sensorDef.shape = circleShape;
+
+    dynamicBody.CreateFixture(sensorDef);
+
+    dynamicBody.SetUserData({type: "7"});
 }
 
 function createBall(x, y, radius, world) {
@@ -217,9 +300,16 @@ function createBall(x, y, radius, world) {
     fixtureDef.friction = 0.3;
     fixtureDef.restitution = 0.2;
 
+    const sensorDef = new Box2D.Dynamics.b2FixtureDef();
+    sensorDef.shape = circleShape;
+    sensorDef.isSensor = true;
+
 
     dynamicBody.CreateFixture(fixtureDef);
+    dynamicBody.CreateFixture(sensorDef);
     dynamicBody.SetAngularDamping(0.5); //stop from spinning forever
+
+    dynamicBody.SetUserData({type: "redball"});
 
     return dynamicBody;
 }
@@ -278,7 +368,6 @@ function applyForceToBall(keys, ball, dt=1/60) {
             dragForceY = -velocity.y;
         }
     }
-console.log(velocity);
     // Apply accel forces
     ball.ApplyForce(
         new Box2D.Common.Math.b2Vec2(forceX, forceY), // Force vector
@@ -593,24 +682,42 @@ function texture(){
     });
 }
 
-function loop(delta, playerSprite, playerCollision, world, keys, app) {
-    applyForceToBall(keys, playerCollision);
+function loop(delta, player, world, keys, app) {
+    applyForceToBall(keys, player.playerCollision);
     world.Step(1 / 60, 8, 3); // Update Box2D world
 
     // Update PixiJS sprite positions
-    const position = playerCollision.GetPosition();
-    const angle = playerCollision.GetAngle();
+    const position = player.playerCollision.GetPosition();
+    const angle = player.playerCollision.GetAngle();
 
-    playerSprite.x = position.x * 40; // Convert from Box2D units to pixels
-    playerSprite.y = position.y * 40;
-    playerSprite.rotation = angle;
+    if(player.dead){
+        if(player.playerSprite.visible){ //we only want to start the countdown once
+            player.playerSprite.visible = false;
+            setTimeout(() => {
+                player.playerSprite.visible = true;
+                player.dead = false;
+                player.playerSprite.x = 120;
+                player.playerSprite.y = 120;
+                player.playerSprite.rotation = 0;
+                console.log(player.playerCollision.m_xf.position);
+                player.playerCollision.SetPosition(new Box2D.Common.Math.b2Vec2(3,3));
+                player.playerCollision.SetLinearVelocity(new Box2D.Common.Math.b2Vec2(0,0));
+            }, 3000);
+        }
+    }
+    else{
+        player.playerSprite.x = position.x * 40; // Convert from Box2D units to pixels
+        player.playerSprite.y = position.y * 40;
+        player.playerSprite.rotation = angle;
+
+    }
 
     //Center view on ball
     app.stage.position.x = WIDTH/2;
     app.stage.position.y = HEIGHT/2;
     //now specify which point INSIDE stage must be (0,0)
-    app.stage.pivot.x = playerSprite.position.x;
-    app.stage.pivot.y = playerSprite.position.y;
+    app.stage.pivot.x = player.playerSprite.position.x;
+    app.stage.pivot.y = player.playerSprite.position.y;
 
 
 
